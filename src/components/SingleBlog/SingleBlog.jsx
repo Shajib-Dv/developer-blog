473b762064488ccf888a66422291b3185e40918c
Shajib-Dv <mdsojeeb242@gmail.com>
2023-03-31 10:50:11 +0600

    blog customized  done

--- src/components/SingleBlog/SingleBlog.jsx
+++ src/components/SingleBlog/SingleBlog.jsx
@@ -3,20 +3,47 @@
 import React from "react";
 
 const SingleBlog = (props) => {
-  const { authorName, authorImg, title, date, time, tags, banner } = props.blog;
+  const { authorName, authorImg, title, date, time, tags, banner, updateTime } =
+    props.blog;
   //   console.log(authorName);
   return (
     <>
-      <div className="card bg-slate-200 shadow-2xl">
+      <div className="card bg-slate-100 shadow-2xl">
         <figure>
           <img className="w-full" src={banner} alt="Shoes" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{authorName}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
+          <div className="flex justify-between items-center">
+            <div className="flex gap-1 ">
+              <div className="avatar placeholder">
+                <div className="bg-neutral-focus text-neutral-content rounded-full w-12">
+                  <img src={authorImg} alt="" />
+                </div>
+              </div>
+              <div>
+                <h3 className="font-bold text-2xl">{authorName}</h3>
+                <p className="text-gray-500">
+                  {date}
+                  <span> ({updateTime} days ago)</span>
+                </p>
+              </div>
+            </div>
+            <div>
+              <p className="text-gray-500">{time} min read</p>
+            </div>
+          </div>
+          <div>
+            <h1 className="text-4xl">{title}</h1>
+          </div>
+          <div className="card-actions justify-start py-2">
+            {tags.map((tag) => (
+              <div className="text-gray-500">#{tag}</div>
+            ))}
+          </div>
+          <div>
+            <button className="text-blue-600 underline font-bold">
+              Mark as Read
+            </button>
           </div>
         </div>
       </div>
